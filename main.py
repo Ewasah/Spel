0d77379b52d6c269b6455bff4867073c235e4337
Ab1603 <93122312+Ewasah@users.noreply.github.com>
2022-06-03 07:17:20 +0000

    for loop gedaan

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ print ("Welkom")
 print("Lucifer game")
 print ("Je speelt tegen een computer die een getal kiest tussen 20 en 25, om de beurt trekt een speler 1,2 of 3 lucifers weg. De speler die het laatste lucifer trekt,verliest!!!" )
 
-def 
 
 
 
