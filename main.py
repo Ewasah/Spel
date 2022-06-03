41de1a39425a1ba3ca6209b952a1190f7711e703
Ab1603 <93122312+Ewasah@users.noreply.github.com>
2022-06-03 07:14:57 +0000

    tekst geschreven

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
 print ("Welkom")
 print("Lucifer game")
-print ("Je speelt tegen een computer die een getal kiest tussen 20 en 25, om de beurt trekt een speler 1,2 of 3 lucifers weg. De speler die het laatste lucifer trekt," )
+print ("Je speelt tegen een computer die een getal kiest tussen 20 en 25, om de beurt trekt een speler 1,2 of 3 lucifers weg. De speler die het laatste lucifer trekt,verliest!!!" )
+
+def 
+
+
+
+
+def game():
+  a = input("Hoeveel lucifers wil je trekken? ")
+  if a == "y":
+    print("Gewonnen :) ")
+  else:
+    print("Verloren :( Probeer het nog eens")
+    game()
+    
+game()
